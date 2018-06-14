33695b9259ec093cb0c7e5816aa5230e49386351
Joseph <josephebenge@gmail.com>
2018-06-14 15:05:14 -0400

    Added save icon

--- ContactBookApp/ContactBookApp/Properties/Resources.Designer.cs
+++ ContactBookApp/ContactBookApp/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace ContactBookApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap saveIcon {
+            get {
+                object obj = ResourceManager.GetObject("saveIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap uncheckedStar {
             get {
                 object obj = ResourceManager.GetObject("uncheckedStar", resourceCulture);
