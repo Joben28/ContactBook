6e17f5e6245bab0a0f4bea051f6cfda6da87f6ec
Joseph <josephebenge@gmail.com>
2018-06-14 15:28:51 -0400

    Added JsonContactDataService

---
+++ ContactBookApp/ContactBookApp/Services/JsonContactDataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactBookApp.Model;
+
+namespace ContactBookApp.Services
+{
+    public class JsonContactDataService : IContactDataService
+    {
+        public IEnumerable<Contact> GetContacts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IEnumerable<Contact> contacts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
